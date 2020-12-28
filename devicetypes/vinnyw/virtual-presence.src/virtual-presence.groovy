bc2169aa29ba0d527f4df6ca38cf7f5baa5a04cc
Vinny Wadding <vinny.wadding@googlemail.com>
2020-12-28 02:25:48 +0000

    wip

--- devicetypes/vinnyw/virtual-presence.src/virtual-presence.groovy
+++ devicetypes/vinnyw/virtual-presence.src/virtual-presence.groovy
@@ -13,37 +13,36 @@
  *  for the specific language governing permissions and limitations under the License.
  */
 metadata {
-	definition (name: "Virtual Presence", namespace: "vinnyw", author: "vinnyw", mcdSync: true, 
-		mnmn: "SmartThings", vid: "generic-arrival-2", ocfDeviceType: "x.com.st.d.sensor.presence" ) {
-//cstHandler: true,
-		capability "Actuator"
-		capability "Switch"        
+	definition (name: "Virtual Presence", namespace: "vinnyw", author: "vinnyw", mcdSync: true, cstHandler: true,
+		mnmn: "SmartThings", vid: "generic-arrival-4", ocfDeviceType: "x.com.st.d.sensor.presence" ) {
+
+		//capability "Actuator"        
 		capability "Presence Sensor"
+		capability "Switch"
         capability "Health Check"
 
-		command "arrive"
-		command "depart"
-
+		//command "arrived"
+		//command "departed"
 	}
 
 	simulator {
 		// TODO
 	}
 
+
 	tiles {
-
-		standardTile("button", "device.switch", decoration: "flat", width: 2, height: 2, canChangeIcon: false, canChangeBackground: true) {
-			state "off", label: 'Away', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#00A0DC"
-			state "on", label: 'Present', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#FFFFFF"
+		standardTile("presence", "device.presence", width: 3, height: 3, canChangeIcon: false, canChangeBackground: true) {
+			state("present", label: '${currentValue} Z', icon:"st.presence.tile.mobile-present", backgroundColor:"#00A0DC")
+			state("not present", label: '${currentValue} W', icon:"st.presence.tile.mobile-not-present", backgroundColor:"#FFFFFF")
+		}
+  
+		standardTile("switch", "device.switch", decoration: "flat", width: 3, height: 3, canChangeIcon: false, canChangeBackground: true) {
+			state("off", action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#00A0DC")
+			state("on", action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#FFFFFF")
 		}
 
-		standardTile("presence", "device.presence", width: 1, height: 1, canChangeBackground: true) {
-			state("present", Icon:"st.presence.tile.mobile-present", backgroundColor:"#53a7c0")
-			state("not present", Icon:"st.presence.tile.mobile-not-present", backgroundColor:"#ffffff")
-		}
-        
- 		main (["button"])
-		details(["button","presense"])
+ 		main(["switch"])
+		details(["presense","switch"])
  
     }
 
@@ -63,33 +62,99 @@ def parse(String description) {
 
 }
 
-def installed() {
-	off()
+def parse(description) {
+	if (deviceDebug) {
+    	writeLog("Executing 'parse()'")
+		writeLog("Parsing '${description}'")
+	}
+
+	def pair = description.split(":")
+	createEvent(name: pair[0].trim(), value: pair[1].trim())
 }
 
+def installed() {
+	if (deviceDebug) {
+		writeLog("Executing 'installed()'")
+		writeLog("installed() settings: $settings", "INFO")
+		writeLog("installed() state: $state", "INFO")
+	}
 
+	off()
+	initialize()
+}
+
+def updated() {
+	if (deviceDebug) {
+		writeLog("Executing 'updated()'")
+		writeLog("updated() settings: $settings", "INFO")
+		writeLog("updated() state: $state", "INFO")
+	}
+
+	initialize()
+}
+
+private initialize() {
+	if (deviceDebug) {
+		writeLog("Executing 'initialize()'")
+	}
+
+	sendEvent(name: "DeviceWatch-Enroll", value: [protocol: "cloud", scheme:"untracked"].encodeAsJson(), displayed: false)
+	sendEvent(name: "DeviceWatch-DeviceStatus", value: "online", displayed: false)
+	sendEvent(name: "healthStatus", value: "online", displayed: false)
+}
 
 def arrived() {
-	on()
+	if (deviceDebug) {
+		writeLog("Executing 'arrived()'")
+	}
+    
+    on()
 }
 
-
 def departed() {
+	if (deviceDebug) {
+		writeLog("Executing 'departed()'")
+	}
+    
     off()
 }
 
 def on() {
-	sendEvent(name: "switch", value: "on")
-    sendEvent(name: "presence", value: "present")
+	if (deviceDebug) {
+		writeLog("Executing 'on()'")
+	}
 
+	if ((device.currentValue("switch") == "on") && !deviceEvent) {
+		if (deviceDebug) {
+			writeLog("no action required.  state is already " + device.currentValue("switch"))
+		}
+		return
+	}
+
+    sendEvent(name: "presence", value: "present", isStateChange: true)
+	sendEvent(name: "switch", value: "on", isStateChange: true, displayed: false)
+
+	if (deviceReset) {
+		runIn(1, "off", [overwrite: true])
+	}
 }
 
 def off() {
-	sendEvent(name: "switch", value: "off")
-    sendEvent(name: "presence", value: "not present")
+	if (deviceDebug) {
+		writeLog("Executing 'off()'")
+	}
+
+	if ((device.currentValue("switch") == "off") && !deviceEvent) {
+		if (deviceDebug) {
+			writeLog("no action required.  state is already " + device.currentValue("switch"))
+		}
+		return
+	}
+
+	unschedule()
+    sendEvent(name: "presence", value: "not present", isStateChange: true)
+	sendEvent(name: "switch", value: "off", isStateChange: true, displayed: false)
 }
-
-
 
 private getState(String value) {
 	switch(value) {
