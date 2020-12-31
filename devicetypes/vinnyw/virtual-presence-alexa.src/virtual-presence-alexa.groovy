f6b4b22a7b3c4b68be91bf43529d072d89e7ee56
Vinny Wadding <vinny.wadding@gmail.com>
2020-12-31 18:40:30 +0000

    1.0.0 presense sensor commit

--- devicetypes/vinnyw/virtual-presence-alexa.src/virtual-presence-alexa.groovy
+++ devicetypes/vinnyw/virtual-presence-alexa.src/virtual-presence-alexa.groovy
@@ -12,15 +12,16 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  */
+//cstHandler: true,
 metadata {
-	definition (name: "Virtual Presence (Alexa)", namespace: "vinnyw", author: "vinnyw", mcdSync: true,
+	definition (name: "Virtual Presence (Alexa)", namespace: "vinnyw", author: "vinnyw", mcdSync: true, 
 		mnmn: "SmartThings", vid: "generic-arrival-4", ocfDeviceType: "x.com.st.d.sensor.presence" ) {
 
 		capability "Actuator"        
 		capability "Presence Sensor"
-		capability "Switch"
 		capability "Contact Sensor"
-        capability "Health Check"
+		capability "Switch"
+		capability "Health Check"
 
 		command "arrived"
 		command "departed"
--- devicetypes/vinnyw/virtual-presence.src/virtual-presence.groovy
+++ devicetypes/vinnyw/virtual-presence.src/virtual-presence.groovy
@@ -11,15 +11,16 @@
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
- */
+ */	//cstHandler: true,
+//cstHandler: true,
 metadata {
-	definition (name: "Virtual Presence", namespace: "vinnyw", author: "vinnyw", mcdSync: true, 
+	definition (name: "Virtual Presence (Alexa)", namespace: "vinnyw", author: "vinnyw", mcdSync: true, 
 		mnmn: "SmartThings", vid: "generic-arrival-4", ocfDeviceType: "x.com.st.d.sensor.presence" ) {
 
 		capability "Actuator"        
 		capability "Presence Sensor"
 		capability "Switch"
-        capability "Health Check"
+		capability "Health Check"
 
 		command "arrived"
 		command "departed"
